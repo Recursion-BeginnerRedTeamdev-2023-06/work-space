d23f3f91b84473f32da9f347bbb6f765e7894454
shka03 <katsurashigeki0325@gmail.com>
2023-06-24 02:00:40 +0900

    TetroMino&Utilityクラスの作成完了。index.htmlをjavascriptでimportするために修正。

--- script/TetroMino.js
+++ script/TetroMino.js
@@ -1,1 +1,47 @@
-class TetroMino {}
+import { Utility } from './Utility.js';
+
+class TetroMino {
+  static BLOCK_SIZE = 30;
+  static MINO_SIZE = 4;
+
+  /** @returns {Array} ランダムで指定された形を2次元配列形式で返す。*/
+  getRandomMinoType() {
+    const SHAPE_PATTERN = ["棒形", "正方形", "S字", "Z字", "J字", "L字", "T字"];
+    let randomShapeType = Math.floor(Math.random() * SHAPE_PATTERN.length);
+    return this.getShape(SHAPE_PATTERN[randomShapeType]);
+  }
+
+  /**
+   * @param {shapeType(Type=String)} pattern 棒形/正方形/S字/Z字/J字/L字/T字
+   * @returns {Array} 指定された形を2次元配列形式で返す。
+   */
+  getShape(shapeType) {
+    const MINO_SHAPE = {
+      "棒形": [ [0, 0, 0, 0], [1, 1, 1, 1],[0, 0, 0, 0], [0, 0, 0, 0] ],
+      "正方形": [ [0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0] ],
+      "S字": [ [0, 0, 0, 0] , [0, 1, 1, 0] , [1, 1, 0, 0] , [0, 0, 0, 0]  ],
+      "Z字": [ [0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0] ],
+      "J字": [ [0, 0, 1, 0] , [0, 0, 1, 0] , [0, 1, 1, 0] , [0, 0, 0, 0]  ],
+      "L字": [ [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0] ],
+      "T字": [ [0, 1, 0, 0], [0, 1, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0] ]
+    };
+
+    return MINO_SHAPE[shapeType];
+  }
+
+  /**
+   * @param {tetro} 回転させたいテトロミノ。
+   * @returns {Array} 回転させた形を2次元配列形式で返す。
+   */
+  static rotate(tetro){
+    let rotateTetro = [];
+
+    for(let y = 0; y < this.MINO_SIZE; y++) {
+      rotateTetro[y] = [];
+      for(let x = 0; x < this.MINO_SIZE; x++){
+        rotateTetro[y][x] = tetro[this.MINO_SIZE-x-1][y];
+      }
+    }
+    return rotateTetro;
+  }
+}
--- script/Utility.js
+++ script/Utility.js
@@ -1,1 +1,36 @@
-class Utility {}
+class Utility {
+    /**
+     * TODO = 相談する。ランダムでMinoを生成している。かつ、本関数は描画=プレイエリアクラスのdrawのように感じた。このクラスで良いか再検討が必要に感じました。
+     * @param {TetroMino} 描画したいMinoの２次元配列
+     * @param {MINO_SIZE} TetroMinoクラスで指定されたミノサイズ
+     * @param {BLOCK_SIZE} TetroMinoクラスで指定されたブロックサイズ
+     */
+    static createMino(tetroMino, MINO_SIZE, BLOCK_SIZE) {
+      const COLOR = ["red", "blue", "green", "orange"]
+      const NON_BLOCKS = 0;
+      
+      let canvas = document.getElementById("canvas");
+      let context = canvas.getContext("2d");
+      let randomNum = Math.floor(Math.random() * COLOR.length);
+  
+      for (let y = 0; y < MINO_SIZE; y++) {
+        for (let x = 0; x < MINO_SIZE; x++) {
+          if (NON_BLOCKS != tetroMino[y][x]) {
+            let position_x = x * BLOCK_SIZE;
+            let position_y = y * BLOCK_SIZE;
+      
+            context.fillStyle = COLOR[randomNum];
+            context.fillRect(position_x, position_y, BLOCK_SIZE, BLOCK_SIZE);
+      
+            context.strokeStyle = "black";
+            context.strokeRect(position_x, position_y, BLOCK_SIZE, BLOCK_SIZE);
+          }
+        }
+      }
+  
+    }
+  
+  }
+  
+  export { Utility };
+  
