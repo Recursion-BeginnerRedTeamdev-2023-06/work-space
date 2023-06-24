85d264430e5ee0cb2bbeb4a8915ede60622c8384
TaiGa02 <d-taiga.4869@outlook.jp>
2023-06-24 14:12:30 +1000

    スコアクラスの作成

--- script/Score.js
+++ script/Score.js
@@ -2,20 +2,20 @@ class Score {
     constructor(line) {
       const BASE_SCORE = 100;       // 1ライン消されると加算されるスコア
       const BASE_BONUS = 2;        // ボーナスの乗数
-      const INIT_SCORE = 0;          // 初期値のスコア
+      let value = 0;          // 初期値のスコア
       this.line = line;
       this.BASE_SCORE = BASE_SCORE;
       this.BASE_BONUS = BASE_BONUS;
-      this.INIT_SCORE = INIT_SCORE;
+      this.value = value;
     }
   
     calculateScore() {
         if (this.line === 1) {
-            this.INIT_SCORE += this.BASE_SCORE; // 消えたライン数が1の時はベースの得点のみが加算される
+            this.value += this.BASE_SCORE; // 消えたライン数が1の時はベースの得点のみが加算される
           } else {
-            this.INIT_SCORE += (this.BASE_SCORE * this.line) + (this.BASE_SCORE * Math.pow(this.BASE_BONUS, this.line - 1));
+            this.value += (this.BASE_SCORE * this.line) + (this.BASE_SCORE * Math.pow(this.BASE_BONUS, this.line - 1));
           } // 一度に複数のラインを消すと、ラインの数に応じてボーナスも加算される
-          return this.INIT_SCORE;
+          return this.value;
     }
     displayScore() {
       let canvas = document.getElementById("canvas");
