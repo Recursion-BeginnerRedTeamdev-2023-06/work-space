d23903ff293df3b52811ccae58581bc26e42542d
hagi845 <gya132811@gmail.com>
2023-06-24 14:10:41 +0900

    gameファイルの追加tuika

---
+++ script/Game.js
@@ -0,0 +1,17 @@
+import { Utility } from './Utility.js';
+import { TetroMino } from './TetroMino.js';
+import { Score } from './Score.js';
+
+let mino = TetroMino.getRandomMinoType();
+
+// 回転させる
+mino =TetroMino.rotate(mino);
+
+// 現状は描画も行っている
+Utility.createMino(mino,TetroMino.MINO_SIZE,TetroMino.BLOCK_SIZE);
+
+// let score = new Score();
+
+// // lineが消える処理の後に記述する想定
+// score.line = 2;
+// score.displayScore();
