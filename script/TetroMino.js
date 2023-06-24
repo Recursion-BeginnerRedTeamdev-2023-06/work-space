6492fa7f6b87294b48c361d12bf461568da2f454
shka03 <katsurashigeki0325@gmail.com>
2023-06-24 11:16:41 +0900

    TetroMinoクラス、コードレビュー結果の修正完了

--- script/TetroMino.js
+++ script/TetroMino.js
@@ -1,32 +1,30 @@
-import { Utility } from './Utility.js';
-
 class TetroMino {
-  #MINO_SHAPE = {
-    "棒形": [ [0, 0, 0, 0], [1, 1, 1, 1],[0, 0, 0, 0], [0, 0, 0, 0] ],
-    "正方形": [ [0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0] ],
-    "S字": [ [0, 0, 0, 0] , [0, 1, 1, 0] , [1, 1, 0, 0] , [0, 0, 0, 0]  ],
-    "Z字": [ [0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0] ],
-    "J字": [ [0, 0, 1, 0] , [0, 0, 1, 0] , [0, 1, 1, 0] , [0, 0, 0, 0]  ],
-    "L字": [ [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0] ],
-    "T字": [ [0, 1, 0, 0], [0, 1, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0] ]
-  };
-
   static BLOCK_SIZE = 30;
+  static MINO_INFO = {
+    "棒形": [ [0, 0, 0, 0], [1, 1, 1, 1],[0, 0, 0, 0], [0, 0, 0, 0], "red" ],
+    "正方形": [ [0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0], "blue" ],
+    "S字": [ [0, 0, 0, 0] , [0, 1, 1, 0] , [1, 1, 0, 0] , [0, 0, 0, 0], "green" ],
+    "Z字": [ [0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0], "orange" ],
+    "J字": [ [0, 0, 1, 0] , [0, 0, 1, 0] , [0, 1, 1, 0] , [0, 0, 0, 0], "brown" ],
+    "L字": [ [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0], "pink" ],
+    "T字": [ [0, 1, 0, 0], [0, 1, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0], "silver" ]
+  };
   static MINO_SIZE = 4;
-
-  /** @returns {Array} ランダムで指定された形を2次元配列形式で返す。*/
-  getRandomMinoType() {
-    const SHAPE_PATTERN = ["棒形", "正方形", "S字", "Z字", "J字", "L字", "T字"];
-    let randomShapeType = Math.floor(Math.random() * SHAPE_PATTERN.length);
-    return this.getShape(SHAPE_PATTERN[randomShapeType]);
-  }
 
   /**
    * @param {shapeType(Type=String)} pattern 棒形/正方形/S字/Z字/J字/L字/T字
    * @returns {Array} 指定された形を2次元配列形式で返す。
    */
-  getShape(shapeType) {
-    return this.#MINO_SHAPE[shapeType];
+  static getShape(shapeType) {
+    return this.MINO_INFO[shapeType];
+  }
+
+  /** @returns {Array} ランダムで指定された形を2次元配列形式で返す。*/
+  static getRandomMinoType() {
+    const SHAPE_PATTERN = ["棒形", "正方形", "S字", "Z字", "J字", "L字", "T字"];
+    let randomShapeType = Math.floor(Math.random() * SHAPE_PATTERN.length);
+
+    return this.getShape(SHAPE_PATTERN[randomShapeType]);
   }
 
   /**
@@ -45,3 +43,5 @@ class TetroMino {
     return rotateTetro;
   }
 }
+
+export { TetroMino }
