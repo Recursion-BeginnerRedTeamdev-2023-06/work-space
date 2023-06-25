217174f5c5a50b3565b33bf4f535a417fbbd5539
shka03 <katsurashigeki0325@gmail.com>
2023-06-25 14:18:12 +0900

    Gameクラスにキー操作のコードを追加

--- script/Game.js
+++ script/Game.js
@@ -21,3 +21,28 @@ let mino = TetroMino.getRandomMinoType();
 // PlayAreaクラスのフィールド描画
 let palyArea = new PlayArea(20, 10, mino);
 palyArea.drawField();
+
+document.addEventListener('keydown',function(e){
+switch(e.keyCode){
+        case 37:
+        // if(checkMove(-1,0,palyArea,tetro["shape"])) palyArea.tetro_x--;
+        palyArea.tetro_x--;
+        break;
+        case 38:
+        // if(checkMove(0,-1,palyArea,tetro["shape"])) palyArea.tetro_y--;
+        palyArea.tetro_y--;
+        break;
+        case 39:
+        // if(checkMove(1,0,palyArea,tetro["shape"])) palyArea.tetro_x++;
+        palyArea.tetro_x++;
+        break;
+        case 40:
+        // if(checkMove(0,1,palyArea,tetro["shape"])) palyArea.tetro_y++;
+        palyArea.tetro_y++;
+        break;
+        case 32:
+        break;   
+    }
+palyArea.drawField();
+// palyArea.context.clearRect(0, 0, palyArea.canvas.height, palyArea.canvas.width);
+});
